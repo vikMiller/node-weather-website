c804175a31e57966165612011a3a8fe6b4f6c77b
VivekVikMiller <tvivek080@gmail.com>
2021-01-07 13:07:13 +0530

    heroku integrated

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('../src/utils/geocode')
 const forecast = require('../src/utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define pathd for Express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -93,6 +94,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
 })
